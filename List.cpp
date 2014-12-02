d3abbb4f5a9c40dc1266c5757a35349836890ad2
Garnier Elodie <egarnier@bs406-s16-03.insa-lyon.fr>
2014-12-02 16:45:06 +0100

    Added function to create a list and to add a element to a list

--- List.cpp
+++ List.cpp
@@ -35,8 +35,8 @@
 // ===========================================================================
 List::List(void)
 {
-	nb_elements=0;
-	head=NULL;
+	nb_elements = 0;
+	head = NULL;
 }
 
 // ===========================================================================
@@ -54,7 +54,34 @@ List::~List(void)
 // ===========================================================================
 //                                Protected Methods
 // ===========================================================================
+void List::NewList(const picture& FirstPicture)
+{
+	nb_elements = 1;
+	Node* FirstNode = new Node(FirstPicture);
+	head = FirstNode;
+}
 
+void List::append(picture NewPicture)
+{
+	if(head == NULL)
+	{
+		printf("First you have to create a NewList before adding a NewElement.\n");
+	}
+	else
+	{
+		Node* cur = new Node();
+		cur=head;
+
+		while(cur->GetNext() != NULL)
+		{
+			cur = cur->GetNext();
+		}
+
+		Node* NewNode = new Node(NewPicture);
+		cur->SetNext(NewNode);
+		nb_elements += 1;
+	}
+}
 // ===========================================================================
 //                               Non inline accessors
 // ===========================================================================
